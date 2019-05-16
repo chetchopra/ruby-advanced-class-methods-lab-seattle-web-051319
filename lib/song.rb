6af615213f4863731ab4b6f2d41d9d89838a6838
Chet  Chopra <chopra.chet@gmail.com>
2019-05-16 18:34:44 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,5 +9,41 @@ class Song
   def save
     self.class.all << self
   end
+  
+  def self.create 
+    song = Song.new
+    @@all << song
+    return song
+  end
+  
+  def self.new_by_name(name)
+    song = Song.new
+    song.name = name
+    @@all << song
+    return song
+  end
+  
+  def self.create_by_name(name)
+    song = Song.new
+    song.name = name
+    @@all << song
+    return song
+  end
+  
+  def self.find_by_name(name)
+    @@all.each do |song|
+      if song.name == name
+        return song
+      end
+    end
+    return FALSE
+  end
+  
+  def self.find_or_create_by_name(name)
+    if !self.find_by_name(name)
+      self.create_by_name(name)
+    end
+  end
+  
 
 end
