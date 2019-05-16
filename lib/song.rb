ed69407381aefd3bdf4f8743fbc913b3825e3f75
Chet  Chopra <chopra.chet@gmail.com>
2019-05-16 18:55:18 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -52,7 +52,7 @@ class Song
   
   #"Thundercat - For Love I Come.mp3"
   def self.new_from_filename(filename)
-    puts filename..trimsplit(/\.|\-|\!/)
+    puts filename.trim.split(/\.|\-|\!/)
   end
 end
 
