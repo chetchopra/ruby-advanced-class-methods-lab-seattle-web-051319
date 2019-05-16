66ccf13b15fd3e42860b92159687b1707faff34f
Chet  Chopra <chopra.chet@gmail.com>
2019-05-16 18:44:17 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -40,11 +40,12 @@ class Song
   end
   
   def self.find_or_create_by_name(name)
-    if !find_by_name(name)
-      create_by_name(name)
-    else 
-      return find_by_name(name)
-    end
+    find_by_name(name) ? find_by_name(name) : create_by_name(name)
+    # if !find_by_name(name)
+    #   create_by_name(name)
+    # else 
+    #   return find_by_name(name)
+    # end
   end
   
 
