3192ce5e726b2832eb0bfca50d622d82001cafaf
Chet  Chopra <chopra.chet@gmail.com>
2019-05-16 19:10:10 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -57,7 +57,12 @@ class Song
   end
   
   def create_from_filename(filename)
-    self.new_from_filename(filename)
+    split_filename = filename.split(/\.|\-/)
+    song = Song.new
+    song.name = split_filename[1].strip
+    song.artist_name = split_filename[0].strip
+    @@all << song
+    return song
   end
 end
 
