0257359f77737dca3e492afb9406a010f8a9faba
Chet  Chopra <chopra.chet@gmail.com>
2019-05-16 18:44:27 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -41,11 +41,6 @@ class Song
   
   def self.find_or_create_by_name(name)
     find_by_name(name) ? find_by_name(name) : create_by_name(name)
-    # if !find_by_name(name)
-    #   create_by_name(name)
-    # else 
-    #   return find_by_name(name)
-    # end
   end
   
 
